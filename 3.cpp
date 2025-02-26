3079b4b16c1e945d6577c0c208ffe7fb3267715f
sicheng xue <1551352060@qq.com>
2025-02-26 21:12:11 +0800

    ，，

--- 3.cpp
+++ 3.cpp
@@ -130,7 +130,7 @@ int main()
     }
 
     //for (int i = 0; i < v1.length(); i++)
-    //    cout << v1[i] <<"."<<endl;666699999111110000
+    //    cout << v1[i] <<"."<<endl;666699999111110000；
 
     return 0;
 }
