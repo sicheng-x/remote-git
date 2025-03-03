b603eadf7049213c5624f5266c429136f0490bb6
sicheng xue <1551352060@qq.com>
2025-03-03 21:24:21 +0800

    a.out

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void phello(){
+	std::cout<<"hello word"<<endl;
+}
+int main(){
+	std::cout<<"--------------------------";
+for(int i=0;i<4;i++){
+	std::cout<<i<<endl;
+
+}
+phello();
+return 0;
+}
